a856edc29faaf389a20767c4be060e03fd61c918
binoyc <binoyc@azuga.com>
2020-04-13 21:48:58 +0530

    Initial commit

---
+++ src/test/java/com/krishna/daa/AppTest.java
@@ -0,0 +1,20 @@
+package com.krishna.daa;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
